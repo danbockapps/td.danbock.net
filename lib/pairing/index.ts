b62d150994942d5acb5e838d0d1f05f4732be098
Dan Bock <danbock@gmail.com>
2026-09-17 07:09:00 -0400

    Add log level env var

--- lib/pairing/index.ts
+++ lib/pairing/index.ts
@@ -3,6 +3,9 @@ import {RatingOrderEngine} from './ratingOrder'
 import {SwissEngine} from './swiss'
 import type {PairingEngine} from './types'
 
+export {getDueColor} from './dueColor'
+export type {DueColor} from './dueColor'
+export {SwissEngine} from './swiss'
 export type {
   PairingEngine,
   PairingInput,
@@ -10,14 +13,11 @@ export type {
   PairingResult,
   RoundHistoryEntry,
 } from './types'
-export {getDueColor} from './dueColor'
-export type {DueColor} from './dueColor'
-export {SwissEngine} from './swiss'
 
 const engines: Record<string, () => PairingEngine> = {
   ratingOrder: () => new RatingOrderEngine(),
   ratingDiffMinimizer: () => new RatingDiffMinimizerEngine(),
-  swiss: () => new SwissEngine(),
+  swiss: () => new SwissEngine({debug: process.env.LOG_LEVEL === '1'}),
 }
 
 export function getPairingEngine(name: string = 'ratingDiffMinimizer'): PairingEngine {
